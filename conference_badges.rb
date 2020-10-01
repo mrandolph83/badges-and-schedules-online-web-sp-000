39a1e49c42b2876af12d7598bc0d8988ef73d271
Matt Smith <mrandolph83@gmail.com>
2020-10-01 18:07:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,6 @@ def assign_rooms(speakers)
 end
 
 def printer (attendees)
-  batch_badge_creator(attendees).each {|badge| puts badge}
-    puts badge
+  batch_badge_creator(attendees).each{|badge| puts badge}
+  assign_rooms(attendees).each{|rooms| puts rooms}
 end
-  
