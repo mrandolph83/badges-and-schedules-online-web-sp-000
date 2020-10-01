b72f6a885c8e6bcbb52cbb61bc64a2c102621c12
Matt Smith <mrandolph83@gmail.com>
2020-10-01 16:48:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,11 @@ def batch_badge_creator(speakers)
   end
   badge_messages
 end
+
+def assign_rooms(speakers)
+  new_arr = []
+  speakers.each_with_index do |name, i|
+  new_arr << "Hello #{name}! You'll be assigned to room #{i+1}!"  
+  end
+  new_arr
+end
